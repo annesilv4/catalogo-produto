efc35996a377c74fbd353eba244a8676b027b059
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-11-18 19:34:51 -0300

    Adicionando o reset page, e importando o newProduto e integrando a rota no botão

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './App.css';
-import { Header } from './components/header/header'
+import { Header } from "./components/header/header";
+import { Produto } from './components/produto/produto';
+import { NewProduto } from './components/newProduto/newProduto';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+    return (
+        <main>
+            <Header />
 
-  return (
-    <>
-      <Header/>
-    </>
-  )
+            <Routes>
+                <Route path="/" element={<Produto />} />
+                <Route path="/newProduto" element={<NewProduto />} />
+            </Routes>
+        </main>
+    )
 }
 
-export default App
+export default App;
+
