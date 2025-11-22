20afbf53eca80d3894b5c16bcccca1046506a2e3
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-11-22 11:08:26 -0300

    Fazendo a estrutura principal do footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import './footer.css';
+
+export function Footer() {
+    return (
+        <footer className='footer__catalogo'>
+            <strong>&copy;2025 - Catalogo de Produto - Todos os direitos reservados.</strong>
+        </footer>
+    )
+}
