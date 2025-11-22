70e672bb34d6987326e579ec9e7539a760d3f3eb
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-11-22 18:35:53 -0300

    Estilizando o footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,6 +4,8 @@ export function Footer() {
     return (
         <footer className='footer__catalogo'>
             <strong>&copy;2025 - Catalogo de Produto - Todos os direitos reservados.</strong>
+            <hr className='separador'/>
+            <p>Feito orgulhosamente por Anne Carolayne</p>
         </footer>
     )
 }
