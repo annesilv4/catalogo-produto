e9069152c1dd441eb1c20974db0accc381a1203e
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-11-23 17:30:41 -0300

    Adicionando a função do Formulário de Cadastro

--- src/pages/newProduto/newProduto.jsx
+++ src/pages/newProduto/newProduto.jsx
@@ -1,15 +1,11 @@
 import './newProduto.css';
 import { Link } from 'react-router-dom';
-
+import { FormNewProduto } from '../../components/formProduto/formProduto';
 export function NewProduto() {
     return (
         <>
-        <h1 className='newProduto__title'>Cadastramento de Produtos</h1>
-        <div className='Produto__Link'>
-            <Link to="/" className="btn__produto">
-                Voltar para Produtos
-            </Link>
-        </div>
+            <h1 className='newProduto__title'>Cadastramento de Produtos</h1>
+            <FormNewProduto />
         </>
     );
 }
