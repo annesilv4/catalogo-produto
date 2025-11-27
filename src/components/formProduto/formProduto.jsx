fcfe28100bbb5ebfbaaac161e4aa952ae7a055e1
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-11-27 18:55:26 -0300

    Adicionando a importação da api de adicionar produto, além da verificação se está salvo o produto, e função para enviar o formulário

--- src/components/formProduto/formProduto.jsx
+++ src/components/formProduto/formProduto.jsx
@@ -1,12 +1,22 @@
 import { useRef, useState } from "react";
 import "./formProduto.css";
 import { Link } from "react-router-dom";
+import { adicionarProduto } from "../../api/apiProduto"; // IMPORTANDO FUNÇÃO DE API
 
 export function FormNewProduto() {
     const [preview, setPreview] = useState(null);
     const [info, setInfo] = useState("Tamanho: - 3:4 Formato: - .png, .jpg");
     const [hasImage, setHasImage] = useState(false);
     const [removeMode, setRemoveMode] = useState(false);
+    // Verificar se o produto foi cadastrado
+    const [isSaved, setIsSaved] = useState(false);
+
+    // DADOS DO PRODUTO
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [sellerName, setSellerName] = useState("");
+    const [description, setDescription] = useState("");
+    const [currency, setCurrency] = useState("R$");
     const [phone, setPhone] = useState("");
 
     const inputRef = useRef();
@@ -88,8 +98,37 @@ export function FormNewProduto() {
         setPhone(formatPhone(e.target.value));
     }
 
+    // FUNCIONALIDADE DO FORMULARIO
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        if (!preview) {
+            alert("Por favor, adicione uma imagem");
+            return;
+        }
+
+        const produto = {
+            imagem: preview,
+            nome: name,
+            descricao: description,
+            preco: price,
+            vendendor: sellerName,
+            telefone: phone,
+            moeda: currency
+        };
+
+        try {
+            await adicionarProduto(produto);
+            alert("Produto cadastrado com sucesso!");
+            setIsSaved(true);
+        } catch (erro) {
+            console.error("Erro ao cadastrar Produto", erro);
+            alert("Falha ao cadastrar produto!");
+        }
+    }
+
     return (
-        <form action="" className="newProduto__form">
+        <form action="" className="newProduto__form" onSubmit={handleSubmit}>
             <div className="form__top">
                 <div className="form__image">
                     <label className="image__text">Imagem do Produto</label>
@@ -120,29 +159,29 @@ export function FormNewProduto() {
                 <div className="form__column">
                     <div className="form__nameProduct">
                         <label htmlFor="name__product" className="name__product">Nome do Produto</label>
-                        <input type="text" id="name__product" className="name__input" />
+                        <input type="text" id="name__product" className="name__input" value={name} onChange={(e) => setName(e.target.value)} />
                     </div>
 
                     <div className="form__priceProduct">
                         <label htmlFor="price__product" className="price__product">Valor do Produto</label>
-                        <input type="text" id="price__product" className="price__input" />
+                        <input type="text" id="price__product" className="price__input" value={price} onChange={(e) => setPrice(e.target.value)} />
                     </div>
 
                     <div className="form__sellerName">
                         <label htmlFor="seller__name" className="seller__name">Nome do Vendendor</label>
-                        <input type="text" id="seller__name" className="sellerName__input" />
+                        <input type="text" id="seller__name" className="sellerName__input" value={sellerName} onChange={(e) => setSellerName(e.target.value)} />
                     </div>
                 </div>
 
                 <div className="form__column">
                     <div className="form__descriptionProduct">
                         <label htmlFor="description__product" className="description__product">Descrição do Produto</label>
-                        <input type="text" id="description__product" className="description__input" />
+                        <input type="text" id="description__product" className="description__input" value={description} onChange={(e) => setDescription(e.target.value)} />
                     </div>
 
                     <div className="form__currencyValue">
                         <label htmlFor="currency__value" className="currency__value">Moeda:</label>
-                        <select id="currency__valueOptions">
+                        <select id="currency__valueOptions" value={currency} onChange={(e) => setCurrency(e.target.value)}>
                             <option value="R$">R$</option>
                             <option value="$">$</option>
                             <option value="£">£</option>
@@ -156,7 +195,11 @@ export function FormNewProduto() {
                 </div>
             </div>
 
-            <Link className="add-product" to="/">Adicionar Produto</Link>
+            {!isSaved ? (
+                <button type="submit" className="btn-addProduct">Adicionar Produto</button>
+            ) : (
+                <Link className="add-product" to="/">Ir para Produtos</Link>
+            )}
         </form>
     );
 }
