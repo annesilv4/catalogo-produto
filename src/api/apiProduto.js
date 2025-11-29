768bd2d07fca7a10f3eba8cec5bd8d7b27ea5cfc
annesilv4 <carolaynebarbozasilva@gmail.com>
2025-11-29 10:40:14 -0300

    Trocando a url da api

--- src/api/apiProduto.js
+++ src/api/apiProduto.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiProduct =
-  "https://crudcrud.com/api/2f5e83d8b5e24dff8b4931f33d6af870/produto";
+  "https://crudcrud.com/api/998abea27d0e47bfb9462155b572e578/produto";
 
 export async function listarProdutos() {
   return axios.get(apiProduct);
